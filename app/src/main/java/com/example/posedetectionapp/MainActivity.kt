fc0d871ef39854b0a9c59e35c251ef15f17daa40
ritik <ritikbhatt020@gmail.com>
2023-10-17 08:53:29 +0530

    changes made

--- app/src/main/java/com/example/posedetectionapp/MainActivity.kt
+++ app/src/main/java/com/example/posedetectionapp/MainActivity.kt
@@ -1,11 +1,130 @@
 package com.example.posedetectionapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import com.example.posedetectionapp.ml.LiteModelMovenetSingleposeLightningTfliteFloat164
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.DataType.*
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var textureView: TextureView
+    lateinit var cameraManager: CameraManager
+    lateinit var imageView: ImageView
+    lateinit var bitmap: Bitmap
+    lateinit var model: LiteModelMovenetSingleposeLightningTfliteFloat164
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getPermissions()
+
+        model = LiteModelMovenetSingleposeLightningTfliteFloat164.newInstance(this)
+        textureView = findViewById(R.id.textureView)
+//        Requesting for camera service
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+        imageView = findViewById(R.id.imageView)
+
+        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+
+// Creates inputs for reference.
+                val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 192, 192, 3), DataType.UINT8)
+                inputFeature0.loadBuffer(byteBuffer)
+
+// Runs model inference and gets result.
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+// Releases model resources if no longer used.
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun openCamera(){
+
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object:CameraDevice.StateCallback(){
+            override fun onOpened(p0: CameraDevice) {
+                var captureRequest = p0.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                var surface = Surface(textureView.surfaceTexture)
+                captureRequest.addTarget(surface)
+
+                p0.createCaptureSession(listOf(surface), object:CameraCaptureSession.StateCallback(){
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+
+                    }
+                }, handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+                TODO("Not yet implemented")
+            }
+        }, handler)
+    }
+
+    fun getPermissions(){
+        if(checkSelfPermission(android.Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermissions()
+        }
     }
 }
